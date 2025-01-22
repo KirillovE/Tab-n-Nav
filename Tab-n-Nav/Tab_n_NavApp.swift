6c018dc7dac8b8a007c1c4893185bdffdb32ac0d
Eugène <Eugene.Kiriloff@iCloud.com>
2025-01-22 10:45:15 +0100

    Initial Commit

---
+++ Tab-n-Nav/Tab_n_NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_n_NavApp.swift
+//  Tab-n-Nav
+//
+//  Created by Eugène Kiriloff on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tab_n_NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
