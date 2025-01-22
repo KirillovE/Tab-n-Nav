7717471b770ca81cf9f2cb3eb70032c743f5a2bc
Eugène <Eugene.Kiriloff@iCloud.com>
2025-01-22 14:39:49 +0100

    Using older API for the animations

--- Tab-n-Nav/ContentView.swift
+++ Tab-n-Nav/ContentView.swift
@@ -4,29 +4,29 @@ struct ContentView: View {
   @State private var selectedTab = 0
   
   var body: some View {
-    Group {
-      ZStack {
-        if selectedTab == 0 {
-          ListViewControllerRepresentable(listType: .list1)
-            .transition(.move(edge: .leading))
-        } else {
-          ListViewControllerRepresentable(listType: .list2)
-            .transition(.move(edge: .trailing))
+    withAnimation {
+      Group {
+        ZStack {
+          if selectedTab == 0 {
+            ListViewControllerRepresentable(listType: .list1)
+              .transition(.move(edge: .leading))
+          } else {
+            ListViewControllerRepresentable(listType: .list2)
+              .transition(.move(edge: .trailing))
+          }
         }
+        .animation(.snappy, value: selectedTab)
+        
+        Spacer(minLength: .zero)
+        
+        Picker("Tabs", selection: $selectedTab) {
+          Text("Jedi").tag(0)
+          Text("Sith").tag(1)
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        .padding()
       }
-      .animation(.snappy, value: selectedTab)
-      
-      Spacer(minLength: .zero)
-      
-      Picker("Tabs", selection: $selectedTab) {
-        Text("Jedi").tag(0)
-        Text("Sith").tag(1)
-      }
-      .pickerStyle(SegmentedPickerStyle())
-      .padding()
-    }
-    .animation(.easeInOut) { group in
-      group.background(
+      .background(
         selectedTab == 0 ? Color.blue.opacity(0.5) : .red.opacity(0.5)
       )
     }
